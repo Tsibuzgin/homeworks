e580551d8342858fc84bbbc0852796d5c0b5181f
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-04-15 22:25:48 +0300

    init

---
+++ hw10/js/script.js
@@ -0,0 +1,50 @@
+// 'use strict'
+//Фрагменты кода исполняйте по очереди
+let a;
+a = 0;
+//1
+// a === 0 ? console.log('Верно') : console.log('Неверно');
+//2
+// a > 0 ? console.log('Верно') : console.log('Неверно');
+//3
+// a < 0 ? console.log('Верно') : console.log('Неверно');
+//4
+// a >= 0 ? console.log('Верно') : console.log('Неверно');
+//5
+// a <= 0 ? console.log('Верно') : console.log('Неверно');
+//6
+// a === 0 ? console.log('Неверно') : console.log('Верно');
+//7
+// a === 'test' ? console.log('Верно') : console.log('Неверно');
+//8
+// a === '1' ? console.log('Верно') : console.log('Неверно');
+//9
+// (a > 0) && (a < 5) ? console.log('Верно') : console.log('Неверно');
+//10
+// (a === 0) || (a === 2) ? a += 7 : a /= 10;
+// console.log(a);
+//11
+let b;
+b = 6;
+// (a <= 1) && (b >= 3) ? console.log(a+b) : console.log(a-b);
+//12
+// ((a > 2) && (a < 11)) || ((b >= 6) && (b < 14)) ? console.log('Верно') : console.log('Неверно');
+//13
+// let num;
+// let result;
+// num = 4;
+// switch (num) {
+//     case 1:
+//         result = 'зима';
+//         break;
+//     case 2:
+//         result = 'весна';
+//         break;
+//     case 3:
+//         result = 'лето';
+//         break;
+//     case 4:
+//         result = 'осень';
+//         break;
+// }
+// console.log(result);
