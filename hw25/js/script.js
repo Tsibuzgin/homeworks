ca9b3393ae8e2bf29f962b770923e1ce2967736b
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-05-24 14:34:00 +0300

    init

--- hw25/js/script.js
+++ hw25/js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-var adder = (function () { var res = 0; return function(x) { res += x; return res; } })();
+let adder = (function () { let res = 0; return function(x) { res += x; return res; } })();
 
 console.log(adder(3));
 console.log(adder(5));
