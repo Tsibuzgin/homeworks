d43ec7f970e70caf05c8ea85ee32ccf4bf69d3fb
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-05-29 16:25:09 +0300

    init

--- hw30/js/script.js
+++ hw30/js/script.js
@@ -1,7 +1,7 @@
-let bind = function(fn, context) {
+let bind = function() {
     // truncate unnecessary arguments (function and context)
     let bindArgs = [].slice.call(arguments, 2);
-    return function() {
+    return function(fn, context) {
       // here all arguments will be needed
       let fnArgs = [].slice.call(arguments);
       // collect everything 
