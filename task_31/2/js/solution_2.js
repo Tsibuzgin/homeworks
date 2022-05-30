7a9f0f4f23de88bf7ff8d5e79a9a678ce1b1b707
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-05-30 10:21:13 +0300

    init

--- task_31/2/js/solution_2.js
+++ task_31/2/js/solution_2.js
@@ -1,4 +1,3 @@
 'use strict';
 let bodyEl = document.body;
-let bodyChilds = bodyEl.childNodes;
 console.log(bodyEl.childNodes[1].children.length);
--- task_31/3/js/solution_3.js
+++ task_31/3/js/solution_3.js
@@ -1,8 +1,5 @@
 'use strict';
 let bodyEl = document.body;
-let bodyChilds = bodyEl.childNodes;
 let list_arr = bodyEl.childNodes[1].children;
 let Arr = Array.from(list_arr);
-for(let element of Arr) {
-    console.log(element.innerText);
-}
+console.log(Arr);
