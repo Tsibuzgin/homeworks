f679ac69140bbdf2d72590bd25f650a18b74b4d6
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-06-02 03:22:49 +0300

    init

---
+++ hw34/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+function createTable() {
+    for (let i = 1; i < 11; i++) {
+        let pre = document.createElement('pre');
+        let p = document.createElement('p');
+        pre.innerText = '';
+        p.innerText = '';
+        for (let j = 10*(i-1) + 1; j < 10*(i-1) + 11; j++) {
+            if (i === 1) {
+                pre.innerText = pre.innerText  + j.toString() + '  ';
+            } else {
+                p.innerText = p.innerText + ' ' + j.toString();
+            }
+        }
+        document.body.append(pre);
+        document.body.append(p);
+    }
+}
+
+createTable(); 
